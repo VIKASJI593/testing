238262436864eb3782102e29433406ced0d99e5f
VIKAS KUMAR <kumar.vikas593@gmail.com>
2022-10-29 00:51:26 +0530

    final cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,80 +1,47 @@
-
 import React from 'react';
 
-class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-     }
-  increaseQuantity = () => {
-  
-     // setState form 2 - if prevState required use this
-     this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1
-      }
-    },() =>  {
-      console.log('this.state', this.state);
-  });
-}
-  
-  decreaseQuantity = () => {
-    const { qty } = this.state;
-    if (qty === 0) {
-      return;
-    }
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty - 1
-      }
-    },() =>  {
-      console.log('this.state', this.state);
-    });
- }
-
-  render () {
-    console.log("state");
-    const { price, title, qty } = this.state;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-         <img style={styles.image} alt = ""  
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+      <img style={styles.image} alt = ""  
          src="https://img.icons8.com/color/48/000000/rectangle.png "/> 
         </div>
-        <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
               alt="increase"
               className="action-icons"
-              src="https://img.icons8.com/ios-glyphs/30/000000/plus--v1.png"
-              onClick={this.increaseQuantity}
+              src="https://img.icons8.com/ios-glyphs/30/000000/plus.png"
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://img.icons8.com/ios-glyphs/30/000000/minus.png"
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
-              src="https://img.icons8.com/fluency/48/000000/filled-trash.png"
+              src="https://img.icons8.com/carbon-copy/100/000000/filled-trash.png"
+              onClick={() => onDeleteProduct(product.id)}
             />
-          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const styles = {
@@ -87,5 +54,3 @@ const styles = {
 }
 
 export default CartItem;
-
-
