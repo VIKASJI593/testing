39ddb842d3eca57e0730c296be55df4fd6a735a3
VIKAS KUMAR <kumar.vikas593@gmail.com>
2022-10-29 11:34:25 +0530

    error in code

--- src/Cart.js
+++ src/Cart.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-const Cart = (props) => {
-  const { products } = props;
-  return (
-    <div className="cart">
-      {products.map((product) => {
-        return (
-          <CartItem
-            product={product}
-            key={product.id}
-            onIncreaseQuantity={props.onIncreaseQuantity}
-            onDecreaseQuantity={props.onDecreaseQuantity}
-            onDeleteProduct={props.onDeleteProduct}
-          />
-        )
-      })}
-    </div>
-  );
+class Cart extends React.Component {
+  render () {
+    const arr = [1, 2, 3, 4, 5];
+    return (
+      <div className="cart">
+        { arr.map((item) => {
+          return item + 5
+        }) }
+      </div>
+    );
+  }
 }
 
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,26 +1,53 @@
 import React from 'react';
 
-const CartItem = (props) => {
-  const { price, title, qty } = props.product;
-  const {
-    product,
-    onIncreaseQuantity,
-    onDecreaseQuantity,
-    onDeleteProduct
-  } = props;
-  return (
-    <div className="cart-item">
+class CartItem extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: ''
+    }
+    
+  }
+  increaseQuantity = () => {
+   
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
+    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
+  render () {
+    console.log('render');
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
       <div className="left-block">
-      <img style={styles.image} alt = ""  
-         src="https://img.icons8.com/color/48/000000/rectangle.png "/> 
-        </div>
-      <div className="right-block">
-        <div style={ { fontSize: 25 } }>{title}</div>
-        <div style={ { color: '#777' } }>Rs {price} </div>
-        <div style={ { color: '#777' } }>Qty: {qty} </div>
-        <div className="cart-item-actions">
-          {/* Buttons */}
-          <img
+        <img style={styles.image} src={product.img} />
+      </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
               alt="increase"
               className="action-icons"
               src="https://img.icons8.com/ios-glyphs/30/000000/plus.png"
